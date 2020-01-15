af2dca2a62e477db79ad73a876ea62458efcb763
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-01-15 15:08:37 +0700

    photo only

--- app/main.py
+++ app/main.py
@@ -38,16 +38,12 @@ def get_s3_image(uri: str):
     return Image.open(img_stream)
 
 
-@app.get("/_api/photo", response_model=Iresponse_photo)
+@app.get("/_api/photo", )
 def get_photo(name: str, bucket: str="face-image"):
     file_path = "s3://"+bucket+"/"+name
     try:
         data_uri = image_to_data_uri(get_s3_image(file_path))
-        return {
-            "photo_data_uri": data_uri,
-            "file_name": name,
-            "bucket_name": bucket
-        }
+        return data_uri
     except:
         raise HTTPException(status_code=404, detail="Photo " +
                             name + " not found in bucket "+bucket)
